51d35ae7df1881d7f0501aaf8f432209494e444b
Eriks Vitolins <evitolins@gmail.com>
2025-09-11 10:43:33 -0700

    Update personal icon and add blur to scatterImages

--- js/scatterImages.js
+++ js/scatterImages.js
@@ -7,6 +7,29 @@ function randomWithinRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+/**
+ * Remap a value from one numeric range to another.
+ * @param {number[]} fromRange - [min, max] of the source range
+ * @param {number[]} toRange - [min, max] of the target range
+ * @param {number} value - value to remap
+ * @returns {number} remapped value
+ */
+function remap(fromRange, toRange, value, isClamped) {
+  const [fromMin, fromMax] = fromRange;
+  const [toMin, toMax] = toRange;
+
+  // Calculate the ratio of the input within the source range
+  const ratio = (value - fromMin) / (fromMax - fromMin);
+
+  if (isClamped) {
+    const clamped = Math.min(Math.max(ratio, 0), 1);
+    return toMin + clamped * (toMax - toMin);
+  } else {
+    // Apply the ratio to the target range
+    return toMin + ratio * (toMax - toMin);
+  }
+}
+
 // Set random transforms to matching DOM elements
 /**
  *
@@ -16,9 +39,9 @@ function randomWithinRange(min, max) {
  *  'div.tech-icon-container > img',
  *  {
  *      maxRotation: 40,
- *      translateX: 10,
- *      translateY: 10,
- *      scale: 1.1
+ *      maxTranslateX: 10,
+ *      maxTranslateY: 10,
+ *      maxScale: 1.1
  *  },
  *  5
  * );
@@ -35,15 +58,19 @@ function scatterElements(selector = "img", limits, transition = 3) {
     maxScale = 1.4,
   } = limits;
   document.querySelectorAll(selector).forEach((img) => {
-    if (Math.random() > 0.5) {
-      return;
-    }
+    // if (Math.random() > 0.5) {
+    //   return;
+    // }
     const rotate = randomRange(maxRotation);
     const translateX = randomRange(maxTranslateX);
     const translateY = randomRange(maxTranslateY);
     const scale = randomWithinRange(0.6, maxScale);
+    const blur = remap([0.6, maxScale], [5, 0], scale, true);
+    const zIndex = remap([0.6, maxScale], [1, 1000], scale, true);
 
     img.style.transform = `scale(${scale}) rotate(${rotate}deg) translate(${translateX}px, ${translateY}px)`;
+    img.style.filter = `blur(${blur}px)`;
+    img.style.zIndex = zIndex;
     img.style.transition = `all ${transition}s cubic-bezier(0.69,0.00,0.00,1.03)`;
   });
 }
